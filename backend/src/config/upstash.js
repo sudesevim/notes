a1ea7c5447aceadd4b473694e70d2f15fc7027e0
Sude Sevim <sudesevim0@gmail.com>
2025-06-08 15:46:26 +0300

    create home page

---
+++ backend/src/config/upstash.js
@@ -0,0 +1,14 @@
+import { Ratelimit } from '@upstash/ratelimit';
+import { Redis } from '@upstash/redis'
+
+import dotenv from "dotenv";
+
+dotenv.config();
+
+// Create a ratelimiter that allows 5 requests per 10 seconds
+const ratelimit = new Ratelimit({
+    redis: Redis.fromEnv(),
+    limiter: Ratelimit.slidingWindow(5, "10 s"),
+});
+
+export default ratelimit;
