da22d87d9eab72341beb994695619f2ba6ced82c
Sude Sevim <sudesevim0@gmail.com>
2025-06-08 16:42:45 +0300

    add delete functionality

--- frontend/src/components/NoteCard.jsx
+++ frontend/src/components/NoteCard.jsx
@@ -2,9 +2,28 @@ import { PenSquareIcon, Trash2Icon } from 'lucide-react';
 import React from 'react';
 import { Link } from 'react-router';
 import { formatDate } from '../lib/utils';
+import api from '../lib/axios';
+import toast from 'react-hot-toast';
 
-const NoteCard = ({note}) => {
-  return <Link to={"/note/${note._id}"} className='card bg-base-100 hover:shadow-lg transition-all duration-200 border-t-4 border-solid  border-[#00FF9D]'>
+const NoteCard = ({note, setNotes}) => {
+
+    const handleDelete = async (e, id) => {
+        e.preventDefault();
+
+        if(!window.confirm("Are you sure you want to delete this note?")) return;
+
+        try {
+            await api.delete(`/notes/${id}`);
+            toast.success("Note deleted succesfully!");
+            setNotes((prev) => prev.filter(note => note._id !== id))
+        } catch (error) {
+            console.log("Error in handleDelete func.", error)
+            toast.error("Failed to delete note");
+        }
+    }
+
+  return (
+  <Link to={"/note/${note._id}"} className='card bg-base-100 hover:shadow-lg transition-all duration-200 border-t-4 border-solid  border-[#00FF9D]'>
     <div className='card-body'>
         <h3 className='card-title'>{note.title}</h3>
         <p className='text-base-content/70 line-clamp-3'>{note.content}</p>
@@ -14,13 +33,14 @@ const NoteCard = ({note}) => {
             </span>
             <div className='flex items-center gap-1'>
                 <PenSquareIcon className='size-4'/>
-                <button className='btn btn-ghost btn-xs text-error'>
+                <button className='btn btn-ghost btn-xs text-error' onClick={(e) => handleDelete(e, note._id)}>
                     <Trash2Icon className='size-4'/>
                 </button>
             </div>
         </div>
     </div>
   </Link>
+  )
 }
 
 export default NoteCard
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -5,6 +5,7 @@ import RateLimitedUi from '../components/RateLimitedUi';
 import api from '../lib/axios';
 import toast from 'react-hot-toast';
 import NoteCard from '../components/NoteCard';
+import NotesNotFound from '../components/NotesNotFound';
 
 const HomePage = () => {
   const [isRateLimited, setIsRateLimited] = useState(false);
@@ -43,10 +44,13 @@ const HomePage = () => {
    <div className='max-w-7xl mx-auto p-4 mt-6'>
     {loading && <div className='text-center text-primary py-10'>Loading notes...</div>}
 
+    {notes.length === 0 && !isRateLimited && <NotesNotFound/>}
+
+
     {notes.length > 0 && !isRateLimited && (
         <div className='grid grid-cols-1 md:grid-cols-3 lg-grid-cols-3 gap-6'>
             {notes.map(note => (
-                <NoteCard key={note._id} note={note} />
+                <NoteCard key={note._id} note={note} setNotes={setNotes} />
             ))}
         </div>
     )}
